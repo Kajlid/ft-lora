0c0ee51c971c8472da656ef6aadf9921b453d838
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-12-02 07:53:10 +0100

    fix(requirements): install llama_cpp_python via subprocess to fix HuggingFace error

--- chat_ui/app.py
+++ chat_ui/app.py
@@ -1,8 +1,10 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
+import subprocess
 from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
 
+subprocess.run("pip install -V llama_cpp_python==0.3.1", shell=True)
 # Download GGUF model into HF Space storage
 model_path = hf_hub_download(
     repo_id="ft-lora/llama3.2-3b-gguf-q4km",
